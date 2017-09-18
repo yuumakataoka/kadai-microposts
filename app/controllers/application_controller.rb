3fe7fe682f504a3568a8fd358ad4fd1d74ffbad1
eimyver3 <yuuma2.index@gmail.com>
2017-09-18 09:02:51 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   protect_from_forgery with: :exception
+  
+  include SessionHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destoroy'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   root to: "toppages#index"
+
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
 end
