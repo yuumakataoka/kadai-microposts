95525fe4455990085cdfba2720a335964b147992
eimyver3 <yuuma2.index@gmail.com>
2017-09-18 10:07:43 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
   
-  include SessionHelper
+  include SessionsHelper
   
   private
   
@@ -11,5 +11,9 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_url
     end
-  end  
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
   get 'sessions/new'
 
   get 'sessions/create'
@@ -13,4 +17,6 @@ Rails.application.routes.draw do
 
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
