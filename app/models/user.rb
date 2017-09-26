e82ba754b590cfed2042e39d4438ef0186aa8841
eimyver3 <yuuma2.index@gmail.com>
2017-09-26 07:24:07 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,9 @@ class User < ApplicationRecord
   
   def following?(other_user)
     self.followings.include?(other_user)
-  end  
+  end
+  
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
 end
